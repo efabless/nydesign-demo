60c2f0852175a0bf2dc9351f57eedeff9b2b6e9c
jeffdi <jeffdi@efabless.com>
2022-11-30 05:24:58 -0800

    update verilog example

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -44,22 +44,17 @@ module user_proj_example #(
 `endif
 
      // IOs
-    input  [`MPRJ_IO_PADS-1:0] io_in,
-    output [`MPRJ_IO_PADS-1:0] io_out,
-    output [`MPRJ_IO_PADS-1:0] io_oeb
+  input  [37:0] io_in,
+  output [37:0] io_out,
+  output [37:0] io_oeb
 
 );
 
     wire clk;
     wire rstn;
-    
-    wire [15:0] bcd;
+
     wire [13:0] count;
     wire [6:0] led;
-    
-    wire [`MPRJ_IO_PADS-1:0] io_in;
-    wire [`MPRJ_IO_PADS-1:0] io_out;
-    wire [`MPRJ_IO_PADS-1:0] io_oeb;
 
     assign count[13:4] = 10'b0;
 
@@ -70,25 +65,27 @@ module user_proj_example #(
 
     assign io_out[18:12] = led[6:0];
     assign io_oeb[18:12] = 7'b0;
-    
+
+    assign io_out[11:0] = 12'b0;
+    assign io_out[37:19] = 19'b0;
+
+    assign io_oeb[9:0] = 10'b0;
+    assign io_oeb[37:19] = 19'b0;
+
+
     counter c0 (
         .clk(clk),
         .rstn(rstn),
         .out(count[3:0])
     );
-    
-    bin2bcd b0 (
-        .bin(count),
-        .bcd(bcd)
-    );
-    
+
     segment7 s0 (
-        .bcd(bcd[3:0]),
-        .seg(led)
+        .count(count[3:0]),
+        .segments(led)
     );
-  
+
 endmodule
-  
+
 module counter (
         input   clk,
         input   rstn,
@@ -101,59 +98,39 @@ always @ (posedge clk) begin
     else
         out <= out + 1;
 end
-  
+
 endmodule
 
-
-module bin2bcd(
-   input [13:0] bin,
-   output reg [15:0] bcd
-   );
-   
-integer i;
-	
-always @(bin) begin
-    bcd=0;		 	
-    for (i=0;i<14;i=i+1) begin					//Iterate once for each bit in input number
-        if (bcd[3:0] >= 5) bcd[3:0] = bcd[3:0] + 3;		//If any BCD digit is >= 5, add three
-	if (bcd[7:4] >= 5) bcd[7:4] = bcd[7:4] + 3;
-	if (bcd[11:8] >= 5) bcd[11:8] = bcd[11:8] + 3;
-	if (bcd[15:12] >= 5) bcd[15:12] = bcd[15:12] + 3;
-	bcd = {bcd[14:0],bin[13-i]};				//Shift one bit, and shift in proper bit from input 
-    end
-end
-endmodule
 
 module segment7(
-     bcd,
-     seg
+     count,
+     segments
     );
-     
-     //Declare inputs,outputs and internal variables.
-     input [3:0] bcd;
-     output [6:0] seg;
-     reg [6:0] seg;
 
-//always block for converting bcd digit into 7 segment format
-    always @(bcd)
+     //Declare inputs,outputs and internal variables.
+     input [3:0] count;
+     output reg [6:0] segments;
+
+//always block for converting bin count into 7 segment format
+  always @(*)
     begin
-        case (bcd) //case statement
-            0 : seg = 7'b0000001;
-            1 : seg = 7'b1001111;
-            2 : seg = 7'b0010010;
-            3 : seg = 7'b0000110;
-            4 : seg = 7'b1001100;
-            5 : seg = 7'b0100100;
-            6 : seg = 7'b0100000;
-            7 : seg = 7'b0001111;
-            8 : seg = 7'b0000000;
-            9 : seg = 7'b0000100;
-            //switch off 7 segment character when the bcd digit is not a decimal number.
-            default : seg = 7'b1111111; 
+        case(count)
+            //                7654321
+            0:  segments = 7'b0111111;
+            1:  segments = 7'b0000110;
+            2:  segments = 7'b1011011;
+            3:  segments = 7'b1001111;
+            4:  segments = 7'b1100110;
+            5:  segments = 7'b1101101;
+            6:  segments = 7'b1111100;
+            7:  segments = 7'b0000111;
+            8:  segments = 7'b1111111;
+            9:  segments = 7'b1100111;
+            default:
+                segments = 7'b0000000;
         endcase
     end
     
 endmodule
-
 
 `default_nettype wire
